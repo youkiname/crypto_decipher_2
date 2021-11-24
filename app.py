ae843a06b54c098d73a82ab1a9973269644dd08f
youkiname <youkiname@mail.ru>
2021-11-24 18:13:28 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify, abort
+from decipher import calculate_key_length, calculate_offsets, decipher, ALPH
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+def web_controller(text: str, custom_key: str) -> dict:
+    KEY_LENGTH = calculate_key_length(text)
+    offsets = calculate_offsets(text, KEY_LENGTH)
+    presumptive_key = "".join([ALPH[i] for i in offsets])
+    key = custom_key or presumptive_key
+    decipher_text = decipher(text, key)
+    return {
+        "decipherText": decipher_text,
+        "presumptiveKey": presumptive_key,
+        "usedKey": key,
+        "presumptiveOffsets": offsets
+    }
+
+
+@app.route("/c2/api/")
+def api():
+    text = request.args.get("text", default="")
+    key = request.args.get("key", default=None)
+    if not text:
+        abort(400, "text is required param.")
+    return jsonify(web_controller(text, key))
+
+
+@app.route("/c2/form/", methods=['POST', "GET"])
+def home():
+    decipher_text = ""
+    text = ""
+    used_key = ""
+    if request.method == 'POST':
+        text = request.form.get('text', default="")
+        key = request.form.get('key', default=None)
+        result = web_controller(text, key)
+        decipher_text = result['decipherText']
+        used_key = result['usedKey']
+    return f"<form method='POST'><textarea name='text' value='{text}'>{text}</textarea><br><input name='key'></input><button type='submit'>Submit</button></form><p>{decipher_text}</p><p>{used_key}</p>"
+
+
+if __name__ == "__main__":
+    app.run("localhost", port=8779)
---
+++ decipher.py
@@ -0,0 +1,90 @@
+from typing import List
+import sys
+
+
+#       012345678901234567890123456789012
+ALPH = " абвгдежзийклмнопрстуфхцчшщъыьэюя"
+
+S = "чшщдаърещмсувзеьгъеуялиуйссдфлыуэъктмхеуучоы ожтяжпдюьжс ьфссэррчыеф эсксюфи " \
+"лреьлнейчждэяуесщлфбъунзмлсмхесбмрдяфеттшлптлуесужръюаосэлхчнцгяжтдвфдтъстдюфцдщмеуьщфссърттлхр юуусужпбжш сфеп шуегмеф афлтлуесшфиъчщдьякесщлдцъскгмлчсщодчротмхемфярдуъсайъйуспфх ржд руеьъеп шуегметчлз эмефдэше"
+
+
+def get_matrix(S: str, key_length: int = 1) -> List[str]:
+    matrix = []
+    for key_index in range(key_length):
+        matrix.append("")
+        for char_index in range(key_index, len(S), key_length):
+            matrix[key_index] += S[char_index]
+    return matrix
+
+
+def get_index_of_coincidence(S: str) -> float:
+    result = 0
+    n = len(S)
+    for char in ALPH:
+        result += (S.count(char) * (S.count(char) - 1)) / (n * (n - 1))
+    return result
+
+
+def calculate_key_length(S, max_length: int = 8) -> int:
+    avgs = []
+    for key_length in range(1, max_length):
+        # print("Key", key_length)
+        matrix = get_matrix(S, key_length)
+        sum_coincidence = 0
+        for i, row in enumerate(matrix):
+            index_of_coincidence = get_index_of_coincidence(row)
+            sum_coincidence += index_of_coincidence
+            # print("\t", index_of_coincidence)
+        avgs.append(sum_coincidence / len(matrix))
+    return sorted(enumerate(avgs), key=lambda x: x[1], reverse=True)[0][0] + 1
+
+
+def get_top_by_frequency(S: str) -> List[int]:
+    frequency = {}
+    for c in set(S):
+        frequency[c] = S.count(c)
+    return sorted(frequency.items(), key=lambda x: x[1], reverse=True)[0][0]
+
+
+def calculate_offsets(S:str, key_length: int) -> List[int]:
+    result = []
+    matrix = get_matrix(S, key_length)
+    for row in matrix:
+        top_char = get_top_by_frequency(row)
+        result.append(ALPH.index(top_char))
+    return result
+
+
+def decipher(S: str, key: str) -> str:
+    offsets = [ALPH.index(c) for c in key]
+    result = list(S)
+    for i, char in enumerate(result):
+        offset_index = i % len(offsets)
+        offset = offsets[offset_index]
+        new_char_index = (ALPH.index(char) - offset) % len(ALPH)
+        result[i] = ALPH[new_char_index]
+    return "".join(result)
+
+
+def main():
+    KEY_LENGTH = calculate_key_length(S)
+    print("Предположительная длина ключа", KEY_LENGTH)
+    offsets = calculate_offsets(S, KEY_LENGTH)
+    print(f"Предположительные сдвиги {offsets}")
+    presumptive_key = "".join([ALPH[i] for i in offsets])
+    print(f"Предположительный ключ '{presumptive_key}'")
+    try:
+        key = sys.argv[1]
+    except IndexError:
+        key = presumptive_key
+    print(f"Используемый ключ '{key}'")
+
+    print()
+    print("Полученный результат расшифровки:")
+    decipher_text = decipher(S, key)
+    print(decipher_text)
+
+
+if __name__ == "__main__":
+    main()
